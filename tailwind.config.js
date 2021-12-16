70756ca6c147fd1f69670ca6b30ccc95a5180a01
Ashu <designer.himanshu.dhp@gmail.com>
2021-12-16 19:16:44 +0530

    ui with new components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
     extend: {
       fontSize: {
         'f-16': '16px',
+        'f-24': '24px',
         'f-28': '28px',
         'f-42': '42px',
+        'f-60': '60px',
+        'f-62': '62px',
         'f-64': '64px',
         'f-74': '74px',
         
@@ -14,13 +17,15 @@ module.exports = {
       backgroundImage: {
         'hero-pattern': "url('img/hero.jpg')",
         'feature-pattern': "url('img/bitmap.png')",
-        'feature2-pattern': "url('img/bitmap.png')",
+        'feature2-pattern': "url('img/bitmap-copy.png')",
 
       },
       lineHeight: {
         'extra-loose': '2.5',
-        '68': '68px',
         '24': '24px',
+        '34': '34px',
+        '68': '68px',
+        
       },
       fontFamily: {
         'nunito': 'Nunito Sans',
@@ -70,7 +75,8 @@ module.exports = {
           bgCol2: '#222222',
           textCol2: '#42cd8f',
           logocol: '#98c83c',
-          footbrdr: '#979797'
+          footbrdr: '#979797',
+          grayCot: '#f5f5f5'
         },
       }
     },
